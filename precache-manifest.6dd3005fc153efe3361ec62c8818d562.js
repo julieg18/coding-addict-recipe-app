170bfead477710e5f465e8defc21df7780abef55
julieg18 <julieheartcrochet@gmail.com>
2019-07-14 09:32:04 -0500

    Updates

---
+++ precache-manifest.6dd3005fc153efe3361ec62c8818d562.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c75e6e4c44c75c4feb2dc1ea490b12",
+    "url": "/coding-addict-recipe-app/index.html"
+  },
+  {
+    "revision": "5fd7de434162ac361bd3",
+    "url": "/coding-addict-recipe-app/static/css/main.b17128b0.chunk.css"
+  },
+  {
+    "revision": "ddd4e623390ec3facaf3",
+    "url": "/coding-addict-recipe-app/static/js/2.77b90360.chunk.js"
+  },
+  {
+    "revision": "5fd7de434162ac361bd3",
+    "url": "/coding-addict-recipe-app/static/js/main.75b62c85.chunk.js"
+  },
+  {
+    "revision": "8e7c16488b5f36239200",
+    "url": "/coding-addict-recipe-app/static/js/runtime~main.47e45ba6.js"
+  }
+]);
